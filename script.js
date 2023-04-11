c1169599c02516c0c1975e14bd84f84d12ca47c2
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-04-11 17:32:02 +0530

    added lowercase and computerchoice function with userinput error

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("HEllo world");
+function playRound(playerSelection, computerSelection) {}
+
+const playerSelection = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("error");
+  }
+}; //string
+const computerSelection = (getComputerChoice) => {
+  Math.floor(Math.random() * 1);
+  switch (1) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+console.log(
+  `The player chose ${playerSelection}. The Computer chose ${getComputerChoice}`
+);
+
+function game(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    console.log("It's a tie");
+  } else {
+    if (playerSelection == "rock") {
+      computerSelection == "paper";
+      console.log("Paper defeats rock! Computer wins");
+    } else {
+      console.log("Congratulations! You win!");
+    }
+  }
+}
