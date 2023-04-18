b5052b4d630ea04694624d53219cf92cd98efdcf
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-04-19 00:18:52 +0530

    working with a const userinput

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-const game= () =>{
-let mychoice = null;
-let computer = null;
 function checkWinner(userChoice, computer) {
     if (userChoice === computer) {
-    return "It's a tie!";
+    return "tie";
   } //when user choses rock
   else if (userChoice == "rock") {
     if (computer == "scissors") {
-      return "rock defeats scissors! player wins";
+      return "player";
     } else if (computer == "paper") {
-      return "paper defeats rock! Computer win";
+      return "Computer";
     }
   } //when user choses paper
   else if (userChoice == "paper") {
     if (computer == "scissors") {
-      return "scissors defeats paper! computer wins";
+      return "computer";
     } else if (computer == "rock") {
-      return "paper defeats stone! Player wins";
+      return "Player";
     }
   } // when user choses scissors
   else if (userChoice == "scissors") {
     if (computer == "rock") {
-      return "stone defeats scissors! Computer wins";
+      return "Computer";
     } else if (computer == "paper") {
-      return "scissors defeat paper";
+      return "paper";
     }
   }
 }
 
 
-
-mychoice = prompt("Please Enter you choice", );
-function getUserChoice(mychoice) {
+/*mychoice = prompt("Please Enter you choice", );*/
+/*function getUserChoice(mychoice) {
   //  take userinput and check if it is rock/paper/scissor if not then displays error
   let userChoice = mychoice.toLowerCase(); //converting the userinput to lowercase
   if (
@@ -43,20 +39,29 @@ function getUserChoice(mychoice) {
   } else {
     return "error";
   }
-}
+  }
+  */
 //Choosing a number between 1 to 3 and then using it to choose among rock/paper/scissor
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     const computerchoice = options[Math.floor(Math.random() * 3)];
     return computerchoice;
 }
-    
+   
 function playGame() {
-  mychoice = "rock";
-  computer = getComputerChoice();
-  let playerScore;
-    let computerScore; 
+    const result = checkWinner(userChoice,computer);
+    if(result=="tie"){
+	return "It is a tie";
+    }
+    else if(result == "player"){
+	    return "You won";
+    }
+    else{
+	return "Computer wins";
+    }
 }
-    playGame();
-}
-game();
+
+    const userChoice = "rock";
+    const computer = getComputerChoice();
+   console.log(checkWinner(userChoice,computer));
+   console.log(playGame(userChoice,computer));
